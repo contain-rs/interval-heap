8401b856c040d351383f515f888b7b0bdf022134
FlashCat <FlashCat@users.noreply.github.com>
2015-03-20 21:13:58 +0000

    rebuild pages at 13a9644

--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['interval-heap'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='interval-heap/struct.Iter.html' title='interval-heap::Iter'>Iter</a>&lt;'a, T&gt;",];
+implementors['interval-heap'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='interval-heap/struct.Iter.html' title='interval-heap::Iter'>Iter</a>&lt;'a, T&gt;","<a class='stability Stable' title='Stable'></a>impl&lt;T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='interval-heap/struct.IntoIter.html' title='interval-heap::IntoIter'>IntoIter</a>&lt;T&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
