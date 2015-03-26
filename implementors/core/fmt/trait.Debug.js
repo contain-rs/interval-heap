c35a07c48a150b9139128efd48cdc5301bd08b7c
FlashCat <FlashCat@users.noreply.github.com>
2015-03-26 18:23:51 +0000

    rebuild pages at f074212

--- implementors/core/fmt/trait.Debug.js
+++ implementors/core/fmt/trait.Debug.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['interval-heap'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>, C: Compare&lt;T&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='interval-heap/struct.IntervalHeap.html' title='interval-heap::IntervalHeap'>IntervalHeap</a>&lt;T, C&gt;",];
+implementors['interval_heap'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a>, C: Compare&lt;T&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='interval_heap/struct.IntervalHeap.html' title='interval_heap::IntervalHeap'>IntervalHeap</a>&lt;T, C&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
