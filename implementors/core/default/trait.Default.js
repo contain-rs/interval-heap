7574cded577a466fb8fade6015cd15770709137b
FlashCat <FlashCat@users.noreply.github.com>
2015-05-02 16:46:23 +0000

    rebuild pages at 1a42d95

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['interval_heap'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;T, C: Compare&lt;T&gt; + <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='interval_heap/struct.IntervalHeap.html' title='interval_heap::IntervalHeap'>IntervalHeap</a>&lt;T, C&gt;",];
+implementors['interval_heap'] = ["impl&lt;T, C: Compare&lt;T&gt; + <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a>&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='interval_heap/struct.IntervalHeap.html' title='interval_heap::IntervalHeap'>IntervalHeap</a>&lt;T, C&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
